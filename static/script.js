2cc95d8913b5ae8ea5df0af7768e9d86505a02e9
JavierTLoina <tejedajavier063@gmail.com>
2025-05-27 14:43:06 -0400

    hamburguer

--- static/script.js
+++ static/script.js
@@ -24,14 +24,21 @@ document.addEventListener("DOMContentLoaded", () => {
 const hamburger = document.getElementById('hamburger');
 const sideMenu = document.getElementById('sideMenu');
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', (e) => {
+    e.stopPropagation(); 
     const isMenuOpen = sideMenu.style.right === '0px';
-    if (isMenuOpen) {
-        sideMenu.style.right = '-250px'; 
-    } else {
-        sideMenu.style.right = '0px'; 
+    sideMenu.style.right = isMenuOpen ? '-250px' : '0px';
+});
+
+document.addEventListener('click', (e) => {
+    const isClickInsideMenu = sideMenu.contains(e.target);
+    const isClickOnHamburger = hamburger.contains(e.target);
+
+    if (!isClickInsideMenu && !isClickOnHamburger) {
+        sideMenu.style.right = '-250px';
     }
 });
+
 
 
 
